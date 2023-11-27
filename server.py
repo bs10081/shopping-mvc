eaf3c9e2cbbeacc8e99816679bb672ba4febe290
orli1022 <148022824+orli1022@users.noreply.github.com>
2023-11-27 14:50:31 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -11,10 +11,21 @@ logging.basicConfig(level=logging.INFO)  # 設置日誌級別為 DEBUG
 NOTION_API_KEY = os.getenv('NOTION_API_KEY')
 NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
 
+
+# Environment variables for different Notion databases and API keys
+NOTION_API_KEY = os.getenv('NOTION_API_KEY')
+NOTION_DATABASE_ID_PRODUCTS = os.getenv('NOTION_DATABASE_ID_PRODUCTS')
+NOTION_DATABASE_ID_CART = os.getenv('NOTION_DATABASE_ID_CART')
 # Ensure environment variables are set
-if not NOTION_API_KEY or not NOTION_DATABASE_ID:
+if not NOTION_API_KEY or not NOTION_DATABASE_ID_PRODUCTS:
     raise ValueError(
         "Please set the NOTION_API_KEY and NOTION_DATABASE_ID environment variables.")
+# Headers for Notion API requests for the cart
+HEADERS_CART = {
+    "Authorization": f"Bearer {NOTION_API_KEY}",
+    "Notion-Version": "2021-05-13",
+    "Content-Type": "application/json"
+}
 
 # Headers for Notion API
 HEADERS = {
@@ -37,14 +48,14 @@ def serve_static(filename):
 @app.route('/products', methods=['get'])
 def get_products():
     try:
-        url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
+        url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID_PRODUCTS}/query"
         # Ensure this is a post request
-        # 這很重要，要用 post 請求，因為我們要傳遞參數，媽的，在這裡卡很久，GPT 是垃圾，我手動排錯的，幹
+        # 這很重要，回應要用 post 請求，，媽的，在這裡卡很久，GPT 是垃圾，我手動排錯的，幹
         response = requests.post(url, headers=HEADERS)
-
         if response.status_code == 200:
             products_data = response.json()
             products = parse_products(products_data)
+            print(products)
             return jsonify(products)
         else:
             return jsonify({"error": "Unable to fetch products"}), response.status_code
@@ -54,6 +65,28 @@ def get_products():
             {"error": "Unable to fetch products", "details": str(e)})
         response.status_code = 500
         return response
+
+
+# @app.route('/product/<product_id>', methods=['get'])
+# def get_product(product_id):
+#     try:
+#         url = f"https://api.notion.com/v1/pages/{product_id}"
+#         # Ensure this is a post request
+#         # 這很重要，回應要用 post 請求，，媽的，在這裡卡很久，GPT 是垃圾，我手動排錯的，幹
+#         response = requests.post(url, headers=HEADERS)
+
+#         if response.status_code == 200:
+#             product_data = response.json()
+#             product = parse_product(product_data)
+#             return jsonify(product)
+#         else:
+#             return jsonify({"error": "Unable to fetch product"}), response.status_code
+#     except Exception as e:
+#         app.logger.error(f"An error occurred: {e}")
+#         response = jsonify(
+#             {"error": "Unable to fetch product", "details": str(e)})
+#         response.status_code = 500
+#         return response
 
 
 def parse_products(notion_data):
@@ -67,7 +100,7 @@ def parse_products(notion_data):
                 "description": next((text['plain_text'] for text in properties.get("Description", {}).get("rich_text", [])), ""),
                 "category": [category.get("name", "") for category in properties.get("Category", {}).get("multi_select", [])],
                 "stock": properties.get("Stock", {}).get("number", 0),
-                "image_url": next((file.get("name", "") for file in properties.get("Image", {}).get("files", [])), ""),
+                # "image": next((file.get("name", "") for file in properties.get("Image", {}).get("url", [])), ""),
                 "price": properties.get("Price", {}).get("number", 0.0),
                 "notion_url": item.get("url", "")
             }
@@ -81,3 +114,5 @@ def parse_products(notion_data):
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
+# Please ensure that there is a route handler for '/product/<product_id>' to fetch product details from Notion
